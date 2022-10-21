3f478e30a07ac462ab7242e48ec953db7d017151
Boris Gonzalez <boris.gonzalez.macias@gmail.com>
2022-10-21 00:50:19 -0300

    Dropdown and carousel for home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ './views/**/*.erb',],
+  content: [ './views/**/*.erb', './node_modules/tw-elements/dist/js/**/*.js'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tw-elements/dist/plugin')
+  ],
 }
