63273c2e0b30b4ed3d5854eb53a55c62db5b6adc
kevinchicken <kevin@codingchickens.com>
2022-10-21 01:07:34 -0300

    feat: add page contents

--- app.rb
+++ app.rb
@@ -1,9 +1,50 @@
 require 'sinatra'
 
 get '/' do
+  @title = "Inicio"
   erb :index
 end
 
+get '/servicios_natacion' do
+  @title = "Natación"
+  erb :servicios_natacion
+end
+
+get '/servicios_hidrogimnasia' do
+  @title = "Hidrogimnasia"
+  erb :servicios_hidrogimnasia
+end
+
+get '/servicios_masajes' do
+  @title = "Masajes"
+  erb :servicios_masajes
+end
+
+get '/beneficios' do
+  @title = "Beneficios"
+  erb :beneficios
+end
+
+get '/cursos' do
+  @title = "Cursos"
+  erb :cursos
+end
+
 get '/horarios' do
+  @title = "Horarios"
   erb :horarios
+end
+
+get '/precios' do
+  @title = "Precios"
+  erb :precios
+end
+
+get '/contacto' do
+  @title = "Contacto"
+  erb :contacto
+end
+
+post '/check' do
+  
 end
